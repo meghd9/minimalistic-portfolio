97d7915d0ef66b0802ff49d8f087b50d453d0d6d
Megh <meghd9@gmail.com>
2024-12-15 15:08:26 +0000

    Project component added and completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Case } from "@/components/Case";
 import { Hero } from "@/components/Hero";
+import { Project } from "@/components/Project";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Case />
+      <Project />
     </div>
   );
 }
